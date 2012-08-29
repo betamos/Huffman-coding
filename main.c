f7d3d743ffb9ce5ae840e6c9cd9d47d9919ccff6
Didrik Nordström <didrik@betamos.se>
2012-08-29 15:10:37 +1000

    Cleaned up main file

--- main.c
+++ main.c
@@ -12,57 +12,14 @@ typedef unsigned int uint;
 void compress(const char *in, const char *out);
 
 int main(int argc, char* argv[]) {
-  compress("test.txt", "compressed.lol");
-  /*
-  FILE *fp = fopen("test.txt", "r");
-  if (fp == NULL) {
-    printf("test.txt not found\n");
-    return 1;
-  }
-  compress_bytestats* stats = compress_fanalyze_original(fp);
-
+  const char *in = "lipsum", *out = "compressed.lol";
+  compress(in, out);
   
 
-  // Build tree
-  tree_node * t = build_tree(stats);
-  tree_leaf_dump(t);
-
-  int u;
-
-  bit_array_t *table[BYTE_MAP_SIZE];
-  for (u = 0; u < 256; u++) {
-    table[u] = NULL;
-  }
-  tree_to_charcode_table(table, t, NULL);
-
-  bit_array_t *tmp;
-  for (u = 0; u < 256; u++) {
-    tmp = table[u];
-    if (tmp != NULL) {
-      fprintf(stderr, "%c: ", (unsigned char) u);
-      BitArrayDump(tmp, stderr);
-    }
-  }
-
-  // Write bits
-  bit_file_t *bft = BitFileOpen("compressed.lol", BF_WRITE);
-  unsigned char buffer[BUFFER_SIZE], ch;
-  unsigned int i2, c; // Iterator, bytes read counter
-  rewind(fp);
-  BitFilePutUint32(stats->totalcount, bft);
-  while ((c = fread(buffer, sizeof(char), BUFFER_SIZE, fp))) {
-    for (i2 = 0; i2 < c; i2++) {
-      fprintf(stderr,  "%c", ch);
-      ch = buffer[i2];
-      tmp = table[ch];
-      BitFilePutBits(bft, BitArrayGetBits(tmp), BitArraySize(tmp));
-    }
-  }
-  BitFileClose(bft);
-  printf("Success! Reading file again:\n");
+  printf("Compressed %s to %s\n", in, out);
 
   // Read compressed
-  bft = BitFileOpen("compressed.lol", BF_READ);
+  /*bft = BitFileOpen("compressed.lol", BF_READ);
   uint bytesleft;
   tree_node *current_node = t;
   for (bytesleft = BitFileGetUint32(bft); bytesleft > 0;) {
